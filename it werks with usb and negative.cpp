aa59d7991e2a906d20b6fb426370211018761f2b
thenipplephilosophy <cameronrsbh@googlemail.com>
2015-07-09 18:01:18 +0100

    Revert "newest version and all that"
    This reverts commit 8a17beebc48d15cd7763ec1ce033b75ef91de0e0.

--- it werks with usb and negative.cpp
+++ it werks with usb and negative.cpp
@@ -1,169 +1,200 @@
 #include "mbed.h"
 #include "MSCFileSystem.h"
-
-#include <string>
-#include "N5110.h"
-
-#define ENGINE_TEMP_ID      10
-#define WHEEL_SPEED_1_ID    700
-#define WHEEL_SPEED_2_ID    710
-#define WHEEL_SPEED_3_ID    730
-#define WHEEL_SPEED_4_ID    740
-#define FUEL_GAUGE_ID       1800
-
-#define PC_DEBUG 1
 #define FSNAME "msc"
-MSCFileSystem msc(FSNAME);
+#define PC_DEBUG 0
+#define MAX_RPM_DISPLAY 9000
+#define COOLANT_WARNING_THRESHOLD 100
 
-Serial pc(USBTX, USBRX);
-Ticker usbLog;
-Timeout pollChange;
-Timer timePassed;
-DigitalOut led1(LED1);
-DigitalOut led2(LED2);
-CAN can1(p30, p29);
+#include "cmath"
+#include <stdio.h>
+#include <string>
 
-char counter = 0;
-char temp;
-float x;
+MSCFileSystem msc("msc");                                       //USB file
+    
 
-struct canData{
-    float engineTemp;
-    uint8_t fuel;
-    uint8_t wheelSpeed[4];
+Serial pc(USBTX, USBRX); // tx, rx
+Serial msquirt(p9, p10); // tx, rx
+DigitalOut myled(LED1);
+AnalogOut LED_driver(p18);
+DigitalOut LED_CLT(p21);
+
+Timer t;
+Timer serialTimeout;
+char serialBuffer[300];
+int serialBufferEnd = 0;
+char loggerBuffer[300]; //Massive ass logger buffer
+bool fetch;
+int count;
+
+struct car_datastruct{
+    float RPM;
+    float map;
+    float mat;
+    float coolant;
+    float throttle;
+    float battery;
+    float air_fuel_1;
+    float air_fuel_2;
+    float advance;
+    bool engine_status[8];
+    bool injectors_status[7];
 };
 
-canData logBuffer;
-
-void setTempPoll(){
-    //Set the temp sensor to poll at a new frequency, def in us.
-    union{
-        char c[4];
-        float f;
-    };
-    f = 1.0;
-    char send[5];
-    send[0] = 0x01; send[1] = c[0]; send[2] = c[1]; send[3] = c[2]; send[4] = c[3];
-    if(can1.write(CANMessage(500, send, 5))){ pc.printf("polling rate changed"); }
-}
-
-
-
-void bufferLogDataFloat(CANMessage msg, float data){
-    //these functions would be more populated if we didnt only have 6 sensors
-    if(msg.id == ENGINE_TEMP_ID){ logBuffer.engineTemp = data; }
-}
-
-void bufferLogDataChar(CANMessage msg, char* data){
-    if(msg.id == FUEL_GAUGE_ID){ logBuffer.fuel = data[0]; }
-    if(msg.id == WHEEL_SPEED_1_ID){ logBuffer.wheelSpeed[0] = data[0]; }
-    if(msg.id == WHEEL_SPEED_2_ID){ logBuffer.wheelSpeed[1] = data[0]; }
-    if(msg.id == WHEEL_SPEED_3_ID){ logBuffer.wheelSpeed[2] = data[0]; }
-    if(msg.id == WHEEL_SPEED_4_ID){ logBuffer.wheelSpeed[3] = data[0]; }
-}
-
-void bufferLogDataInt(CANMessage msg, int data){
-    //I make all these fancy data types and everyone just uses chars...
-}
-
-void bufferLogDataLong(CANMessage msg, long data){
-    //I am confident that no one will ever send a long.
-}
-
-
-void CANRecivedMessage(CANMessage msg){
-        //This is an ungodly union to convert from the characters to whatever you need.
-        union{
-            char c[4];
-            float f;
-            int32_t i;
-            uint32_t ui;
-        };
-        
-        pc.printf("Message recieved of length %d :  ", msg.len);
-        //Print all the characters from the message in order.
-        for(int j = 0; j < msg.len; j++){
-            pc.printf("%d, ", msg.data[j]);
+car_datastruct car_data;
+     
+                      
+void megasquirtRequest(){
+    
+    //Send request
+    msquirt.putc('A');
+    
+    count++;
+    serialBufferEnd = 0;
+    if(PC_DEBUG){ pc.printf("Reading data\n\r");}
+    
+    //Collect responce into buffer and resend if no responce after an amount of time
+    serialTimeout.reset();
+    while (1){
+        if (msquirt.readable())
+        {
+            serialBuffer[serialBufferEnd] = msquirt.getc();
+            serialBufferEnd++;
         }
-        
-        //fill the union
-        for(int j = 1; (j < 5) && (j < msg.len); j++){
-            c[j - 1] = msg.data[j];
-        }
-        //Print the data type defined by the first character and convery the characters to that type and display. 
-        //The bitwise AND seperates the character to give us only the first two digits.
-        switch(msg.data[0] & 0x03){
-            case 0:
-                //message is a character
-                pc.printf("character: %d ", c[0]);
-                bufferLogDataChar(msg, c);
-                break;
-            case 1:
-                //message is a float
-                pc.printf("float: %f ", f);
-                bufferLogDataFloat(msg, f);
-                break;
-            case 2:
-                //message is an int
-                pc.printf("integer: %d ", i);
-                break;
-            case 3:
-                //message is a long
-                pc.printf("long: %u ", ui);
-                break;
-        }
-        pc.printf("from ID %d\n\r", msg.id);
-        pc.printf("Reed errors:%d\n\r", can1.rderror());
-}
 
-
-void logToUSB(){
-    FILE *fp = fopen( "/msc/can_log.csv", "a"); //open USB file
-    //Write log to file
-    if(fp != NULL){
-        fprintf(fp, "%d,%f,%d,%d,%d,%d,%d,\n\r", timePassed.read(), logBuffer.engineTemp, 
-                                                logBuffer.wheelSpeed[0], logBuffer.wheelSpeed[1], logBuffer.wheelSpeed[2], logBuffer.wheelSpeed[3], 
-                                                logBuffer.fuel);
-        fclose(fp);                         //close USB file
-    } else if(PC_DEBUG){ pc.printf("Write to USB failed\n\r"); }
-}
-
-int main() {
-    pc.baud(115200);
-    led2 = 1;
-    timePassed.start();
-    CANMessage msg;
-    
-    
-    //Set a timer to change the polling rate for the temp sensor after 10 seconds. See if it actually works and all that...
-    pollChange.attach(setTempPoll, 10.0);
-    
-    FILE *fp = fopen( "/msc/can_log.csv", "w"); //open USB file
-    //Write log to file
-    if(fp != NULL){
-        fprintf(fp, "Time,Engine Temperature,Wheel Speed 1,Wheel Speed 2,Wheel Speed 3,Wheel Speed 4,Fuel Gauge\n\r\n\r");
-        fclose(fp);                         //close USB file
-    } else if(PC_DEBUG){ pc.printf("Write to USB failed\n\r"); }
-
-    wait(1);
-    
-    //usbLog.attach(logToUSB, 3);
-    
-    while(1) {
-        led1 = !led1;
-        if(can1.read(msg)) {
-            CANRecivedMessage(msg);
-            led2 = !led2;
-            
-                FILE *fp = fopen( "/msc/can_log.csv", "a"); //open USB file
-    //Write log to file
-    if(fp != NULL){
-        fprintf(fp, "%f,%f,%d,%d,%d,%d,%d,\n\r", timePassed.read(), logBuffer.engineTemp, 
-                                                logBuffer.wheelSpeed[0], logBuffer.wheelSpeed[1], logBuffer.wheelSpeed[2], logBuffer.wheelSpeed[3], 
-                                                logBuffer.fuel);
-        fclose(fp);                         //close USB file
-    } else if(PC_DEBUG){ pc.printf("Write to USB failed\n\r"); }
- 
+        if (serialBufferEnd >= 209){ break; }
+        if (serialTimeout.read_ms() > 100){
+            msquirt.putc('A');
+            serialTimeout.reset();
         }
     }
+
+    union
+    {
+        uint8_t b[2];
+        int16_t i;
+    };
+    
+    
+    b[1] = serialBuffer[22]; b[0] = serialBuffer[23];
+    car_data.coolant = (i-320) * 0.05555;
+    if(car_data.coolant >= COOLANT_WARNING_THRESHOLD){ LED_CLT = 1;} else {LED_CLT = 0;}   //Coolant warning light code
+    if(PC_DEBUG){ pc.printf("CLT = %f \r\n",car_data.coolant);}
+    
+    b[1] = serialBuffer[6]; b[0] = serialBuffer[7];
+    car_data.RPM = i;
+    //LED_driver.write_u16(65535/MAX_RPM_DISPLAY);
+    if(PC_DEBUG){ pc.printf("rpm %f \n\r",car_data.RPM);}
+    
+    b[1] = serialBuffer[28];b[0] = serialBuffer[29];
+    car_data.air_fuel_1 = i/10.0;
+    if(PC_DEBUG){ pc.printf("Air_fuel_1 %f \n\r",car_data.air_fuel_1);}
+    
+    b[1] = serialBuffer[18];b[0] = serialBuffer[19];
+    car_data.map = i*0.1;
+    if(PC_DEBUG){ pc.printf("MAP %f \n\r",car_data.map);}
+    
+    b[1] = serialBuffer[20];b[0] = serialBuffer[21];
+    car_data.mat = (i-320)*0.05555;
+    if(PC_DEBUG){ pc.printf("MAT %f \r\n", car_data.mat);}
+
+    
+    b[1] = serialBuffer[24];b[0] = serialBuffer[25];
+    car_data.throttle = i/10.0;
+    if(PC_DEBUG){ pc.printf("Throttle %f \r\n", car_data.throttle);}
+
+    b[1] = serialBuffer[8];b[0] = serialBuffer[9];
+    car_data.advance = i*0.1;
+    if(PC_DEBUG){ pc.printf("Advance %f \r\n", car_data.advance);}
+    
+    b[1] = serialBuffer[26];b[0] = serialBuffer[27];
+    car_data.battery = i*0.1;
+    if(PC_DEBUG){ pc.printf("Battery %f \r\n", car_data.battery);}
+
+    /*b[0] = serialBuffer[108];
+    car_data.throttle = i*0.1;
+    pc.printf("Throttle %f \r\n", car_data.throttle);*/
+    
+    b[1] = serialBuffer[28];b[0] = serialBuffer[29];
+    car_data.air_fuel_1 = i*0.1;
+    b[1] = serialBuffer[28];b[0] = serialBuffer[29];
+    car_data.air_fuel_2 = i*0.1;
+    int offset;
+    
+    if(PC_DEBUG){ pc.printf("injectors ");}
+    b[0] = serialBuffer[10];
+    for(offset = 0; offset<7; offset++){
+        car_data.injectors_status[offset] = ((b[0] >> offset) & 0x01);
+        if(PC_DEBUG){ pc.printf("%d", car_data.injectors_status[offset]);}
+    }
+    
+    if(PC_DEBUG){ pc.printf("engine ");}
+     b[0] = serialBuffer[11];
+    for(offset = 0; offset<7; offset++){
+        car_data.engine_status[offset] = ((b[0] >> offset) & 0x01);
+        
+    }
+    
+    
+    if(PC_DEBUG){ 
+        pc.printf("got %d bytes\r\n \r\n", serialBufferEnd);
+        pc.printf("TIMEEEE  %f \r\n", t.read());
+    }
+    
+    sprintf(loggerBuffer, "%.2f\t%.0f\t%.3f\t%.1f\t%.1f\t%.1f\t%.1f \r\n\0", t.read(), car_data.RPM, car_data.mat, car_data.map, car_data.coolant, car_data.throttle, car_data.battery );
+
+    fetch = true;
+}
+
+void flush()
+//Removed the character to write to, idk if it will still work, it should, but this is the mbed
+{
+    while(msquirt.readable())
+    {
+        msquirt.getc();
+    }
+}
+
+
+int main() {
+    //Wait for megasquirt to initialise
+    wait(1);
+    //This baud rate does work and has been tested through dodgy dodgy extention cables, but may be flaky so change if neccessary
+    msquirt.baud(115200);
+    pc.baud(115200);
+    LED_driver.write_u16(65535);
+    serialTimeout.start();
+    
+    //File initialisation
+    
+    FILE *fp = fopen( "/msc/usb5.tsv", "w");                    //open USB file
+    if(fp != NULL){
+        pc.printf("\r\nData Logging Started\r\n\r\n");              //print instructions to terminal     
+        fprintf(fp,"TIME\tRPM\tMAT\tMAP\tCLT\tTHROTTLE\tBATTERY\r\n\r\n");   //Print the field names
+        fclose (fp);
+    } else { pc.printf("Write to USB failed\n\r"); }
+    
+    t.start();       
+
+    pc.printf("main\r\n");
+    fetch = true;
+    while(1){
+        if(LED_driver.read() >= 1.0){ LED_driver = 0.0; }
+        LED_driver = LED_driver.read() + 0.1;
+
+        if(fetch){   
+            
+            flush();
+            megasquirtRequest();
+            
+            FILE *fp = fopen( "/msc/usb5.tsv", "a"); //open USB file
+            //Write log to file
+            if(fp != NULL){
+                fprintf(fp, loggerBuffer);
+                fclose(fp);                         //close USB file
+            } else if(PC_DEBUG){ pc.printf("Write to USB failed\n\r"); }
+        }
+    }        
+            
+            
+
 }
